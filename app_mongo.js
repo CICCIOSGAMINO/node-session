15af7abe1667bfa84edc6c00eec9b0b648b0121c
CiccioSgamino <marco.canali@gmail.com>
2014-07-23 17:19:38 +0200

    created

--- app_mongo.js
+++ app_mongo.js
@@ -1,17 +1,17 @@
 // simple session + mongodb testing 
 var express = require('express');
-var session = require('express-session');
+var expressSession = require('express-session');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-// var MongoStore = require('connect-mongo')(express);
+var MongoStore = require('connect-mongo')(expressSession);
 var app = express();
 
 // initialize the cookieParser - ALWAYS before expressSession
 app.use(cookieParser());
 // initialize the session & connect to db 
-app.use(session({
+app.use(expressSession({
 	secret : 'cicciosgamino',
-	,store : new MongoStore({db:'session_app',host:'127.0.0.1',port:27017})
+	store : new MongoStore({db:'session_app',host:'127.0.0.1',port:27017})
 }));
 // initialize the bodyParser 
 app.use(bodyParser());
